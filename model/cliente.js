a98381d27d44f468cd4b5133a4bfbdfeed86858e
Mateus de Miranda Pereira <terrymafa@gmail.com>
2024-06-02 22:37:14 -0300

    correcao da tabela cliente

--- model/cliente.js
+++ model/cliente.js
@@ -10,15 +10,14 @@ const cliente = {
 		const query = `create table if not exists cliente(
 			id int not null primary key auto_increment,
 			nome varchar(255) not null,
-			cpf varchar(15) not null unique,
-			identidade varchar(20) not null,
-			ddd char(3) not null,
-			telefone varchar(10) not null,
+			cpf varchar(14) not null unique,
+			identidade varchar(10) not null,
+			telefone varchar(14) not null,
 			email varchar(255) not null unique,
 			cidade varchar(255) not null,
 			bairro varchar(255) not null,
 			rua varchar(255) not null,
-			cep varchar(10) not null,
+			cep varchar(8) not null,
 			estado varchar(255) not null)`;
 
 		await conn.query(query);
